3494c38ddbdeffeeb0a14d3ccd28a54df29b2127
am4teur <daniel.7c.n12@gmail.com>
2020-10-16 18:47:06 +0100

    Create a simple graph with networkX

--- scripts/joinShapeStop.py
+++ scripts/joinShapeStop.py
@@ -2,11 +2,12 @@ import osmnx as ox
 import networkx as nx
 import matplotlib.pyplot as plt
 
+# GET CURRENT PATH
 PATH = "C:/Users/danie/Desktop/Extras/CRC/Network-Analysis--Lisbon-Public-Transports/"
 
 #parse shape.txt into a vector
 def main():
-    with open(PATH + "data/cp/shapes.txt", "r") as f:
+    with open(PATH + "data/metro/shapes.txt", "r", encoding="utf8") as f:
         shape_data = f.read()
 
     vec_shape = shape_data.split("\n")
@@ -17,7 +18,7 @@ def main():
     del vec_shape[-1]
 
 
-    with open(PATH + "data/cp/stops.txt", "r") as f:
+    with open(PATH + "data/metro/stops.txt", "r", encoding="utf8") as f:
         stops_data = f.read()
 
     vec_stops = stops_data.split("\n")
@@ -27,29 +28,34 @@ def main():
     del vec_stops[0]
     del vec_stops[-1]
 
-    some = 1
+
     for shape in vec_shape:
         for stop in vec_stops:
             if(shape[1] == stop[4] and shape[2] == stop[5]):
                 shape.append(stop[2])
     
-    #print(vec_shape)
 
+    #['401', '38.70085', '-9.41792', '1', '', 'Cascais']
 
+    
     G = nx.Graph()
 
     for shape in vec_shape:
-        if(shape[0] == "401"):
-                G.add_nodes_from([ (shape[3], {"name": shape[5], "x": shape[1], "y": shape[2]}) ])
-        else:
-            break
+        route = int(shape[0])
+        x = shape[1]
+        y = shape[2]
+        id = int(shape[3])
+        location = shape[5]
+        G.add_nodes_from([ (x + ":" + y, {"location": location, "route": route, "id": id, "x": x, "y": y}) ])
     
-    nodes = list(G.nodes)
-    for i in range(len(nodes)-1):
-        G.add_edge(nodes[i], nodes[i+1])
 
-    nodes_dic = dict(G.nodes)
-    print(nodes_dic["1"])
+    nodes = dict(G.nodes)
+    # O(N^2) very bad
+    for k, v in nodes.items():
+        for k2, v2 in nodes.items():
+            if(v["route"] == v2["route"] and (v["id"] == v2["id"]+1 or v["id"] == v2["id"]-1)):
+                G.add_edge(k, k2)
+
     print(G.edges)
 
     
@@ -59,6 +65,11 @@ def main():
 
     plt.show()
     '''
+    place = {'city'   : 'Lisbon',
+            'country': 'Portugal'}
+    G = ox.graph_from_place(place, network_type='drive', truncate_by_edge=True)
+    fig, ax = ox.plot_graph(G, figsize=(10, 10), node_size=0, edge_color='y', edge_linewidth=0.2)
+    
 
     Go = ox.graph.graph_from_point((38.70085,-9.41792))
 
