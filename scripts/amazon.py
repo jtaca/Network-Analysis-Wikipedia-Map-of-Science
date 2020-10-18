d7fa9057f14c8c6861a91dc8c5e6c4d565caf289
am4teur <daniel.7c.n12@gmail.com>
2020-10-18 11:57:35 +0100

    Calculated in and out Degree Distribution

--- scripts/amazon.py
+++ scripts/amazon.py
@@ -14,29 +14,27 @@ def main():
         G = pickle.load(f)
 
 
-    print(len(G.nodes)) # 262111
+    #Z (Characterize the Average Degree)                                     Z = 2E/N undirected graph | Z = E/N directed graph
+    
+    
+    #P(k) (Characterize the Degree Distribution)
+    for i in ["out", "in"]:
+        for j in ["out", "in"]:
+            print(f'x={i:<3}  y={j:<3} => {nx.degree_assortativity_coefficient(G, x=i, y=j, weight=None, nodes=None)}')
+
+
+
+    #print(len(G.nodes)) # 262111
     #print(len(G.edges)) # 899792
 
     #print(nx.average_clustering(G)) # 0.419780014607673
-    #print(nx.average_degree_connectivity(G)) # ~10secs, dict with (probabilities?)
-    #print(nx.average_neighbor_degree(G)) # ~10secs, dict with (values?)
+    #print(nx.average_degree_connectivity(G)) # Z | ~10secs, dict with (probabilities?), do a graph, probably with log scale or power scale
+    #print(nx.average_neighbor_degree(G)) # ~10secs, dict with (values?), do a graph, probably with log scale or power scale
     
     
-    #print(nx.average_shortest_path_length(G)) #takes too long
-    #print(nx.diameter(G)) # takes too long
-    
-    #print(nx.radius(G)) # 
-
-
-    #print(G.nodes)
-    '''
-    Characterize the average degree 
-    and degree distribution of the network. 
-    Clustering coefficient and average path length. 
-
-    Infer the importance (centrality) of each node (choose one relevant centrality measure for the chosen dataset and justify your choice).   
-    Suggest a set of principles responsible for the self-organization and creation of such network topology/topologies
-    '''
+    #print(nx.average_shortest_path_length(G))  # takes too long
+    #print(nx.diameter(G))                      # takes too long
+    #print(nx.radius(G))                        # takes too long
 
 
 def create_graph():
@@ -69,17 +67,34 @@ main()
 
 
 '''
-NAME                                VALUE               CHECKED?
+NAME                                VALUE               CONFIRMED?      Notes:
 
-Nodes                               262111              Y
-Edges 	                            1234877             Y
-Nodes in largest WCC 	            262111 (1.000)          N
-Edges in largest WCC 	            1234877 (1.000)         N
-Nodes in largest SCC 	            241761 (0.922)          N
-Edges in largest SCC 	            1131217 (0.916)         N
-Average clustering coefficient 	    0.4198              Y
-Number of triangles 	            717719                  N
-Fraction of closed triangles 	    0.09339                 N
-Diameter (longest shortest path) 	32                      N
-90-percentile effective diameter 	11                      N
+Nodes                               262111              Y               .
+Edges 	                            1234877             Y               .
+Nodes in largest WCC 	            262111 (1.000)          N           .
+Edges in largest WCC 	            1234877 (1.000)         N           .
+Nodes in largest SCC 	            241761 (0.922)          N           .
+Edges in largest SCC 	            1131217 (0.916)         N           .
+Average clustering coefficient 	    0.4198              Y               .
+Number of triangles 	            717719                  N           .
+Fraction of closed triangles 	    0.09339                 N           .
+Diameter (longest shortest path) 	32                      N           Takes too long
+90-percentile effective diameter 	11                      N           .
+
+
+THINGS TO ADD
+Centrality (various types of Centrality, we only need to choose one but we need to justify why we choosed one. Could be by trying a few or just a nice explanation)
+Z (Characterize the Average Degree)                                     Z = 2E/N undirected graph | Z = E/N directed graph
+P(k) (Characterize the Degree Distribution)
+    x=out, y=out => -0.0024793421263094566
+    x=out, y=in  => -0.0024793421263094566
+    x=in,  y=out => -0.0024793421263094566
+    x=in,  y=in  => -0.0024793421263094566
+
+CC (Clustering Coefficient)         
+APL (Average Path Length)                                               Takes too long
+
+
+    Infer the importance (centrality) of each node (choose one relevant centrality measure for the chosen dataset and justify your choice).   
+    Suggest a set of principles responsible for the self-organization and creation of such network topology/topologies
 '''
